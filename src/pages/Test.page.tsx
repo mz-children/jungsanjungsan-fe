9c01e6f89ea2a5a99485a6c6c5abdf0706ea9ca8
siwon <ksw3721@gmail.com>
2026-08-16 14:10:04 +0900

    TextArea 테스트로 작성

--- src/pages/Test.page.tsx
+++ src/pages/Test.page.tsx
@@ -6,6 +6,7 @@
 // import PersonalLeagueCard from "../components/PersonalLeagueCard";
 
 import CircleButton from "../components/ui/AddButton";
+import TextArea from "../components/ui/TextArea";
 
 function TestPage() {
   // const { data, toggleSNB } = useLayoutStore();
@@ -17,8 +18,10 @@ function TestPage() {
 
   return (
     <div className="flex justify-center items-center h-screen bg-[lightgrey]">
-      <CircleButton />
-      <div className="font-normal font-[13px] border-danger border-1">test</div>
+      {/* <CircleButton /> */}
+
+      <TextArea />
+      {/* <div className="font-normal font-[13px] border-danger border-1">test</div> */}
     </div>
   );
 }
