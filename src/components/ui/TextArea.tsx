92ee3271596f37c65eccaf5bdcff6bc6326c414d
kshe1242-commits <kshe1242@gmail.com>
2026-08-17 16:11:19 +0900

    TextArea 컴포넌트 구현 (#4)
    * feat: add TextArea components
    
    * TextArea 수정
    
    * TextArea 수정 / 스크롤바 안보이게 수정
    
    * TextArea 수정
    
    * reset.css 위치 수정
    
    * 디자인 토큰 추가 수정 폰트는 아직 전부 적용 안했습니다.
    
    * reset.css 최종 수정
    
    * app.css 수정
    
    * 코드 개행 수정
    
    * 그림자 폰트 설정

--- src/components/ui/TextArea.tsx
+++ src/components/ui/TextArea.tsx
@@ -1,3 +1,25 @@
-export default function TextArea() {
-  return <div>TextArea</div>;
+type TextAreaProps = {
+  name: string;
+  value: string;
+  placeholder?: string;
+  onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  className?: string;
+};
+
+export default function TextArea({
+  name,
+  value,
+  placeholder = "",
+  onChange,
+  className = "",
+}: TextAreaProps) {
+  return (
+    <textarea
+      name={name}
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+      className={`w-full min-w-[354px] h-[80px] p-[16px] bg-surface-sub border border-border-default rounded-[8px] text-text-primary text-body-regular leading-[1.15] no-scrollbar placeholder:text-text-dim resize-none ${className}`}
+    />
+  );
 }
--- src/pages/Test.page.tsx
+++ src/pages/Test.page.tsx
@@ -1,4 +1,3 @@
-// import { useState } from "react";
 // import reactLogo from "../assets/react.svg";
 // import viteLogo from "../assets/vite.svg";
 // import heroImg from "../assets/hero.png";
@@ -6,23 +5,33 @@
 // import PersonalLeagueCard from "../components/PersonalLeagueCard";
 
 // import CircleButton from "../components/ui/AddButton";
+
+import { useState } from "react";
 import TextArea from "../components/ui/TextArea";
+import CircleButton from "../components/ui/AddButton";
 
 function TestPage() {
+  const [text, setText] = useState("");
   // const { data, toggleSNB } = useLayoutStore();
   // const [count, setCount] = useState(0);
-
   // const handleClickBtn = () => {
   //   toggleSNB();
   // };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-[lightgrey]">
-      {/* <CircleButton /> */}
-
-      <TextArea />
-      {/* <div className="font-normal font-[13px] border-danger border-1">test</div> */}
-    </div>
+    <>
+      <CircleButton />
+      <div className="flex justify-center items-center h-screen bg-[lightgrey]">
+        {/* <CircleButton /> */}
+        <TextArea
+          name="test"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="테스트 입력창"
+        />
+        {/* <div className="font-normal font-[13px] border-danger border-1">test</div> */}
+      </div>
+    </>
   );
 }
 
