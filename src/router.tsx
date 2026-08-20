ded1db7731391e0762897964984bfabccfdf6b14
madein <xorb811@gmail.com>
2026-08-20 16:34:44 +0900

    button 컴포넌트 구현 (#7)
    * feat: add rectangle button
    
    * fix: Button 컴포넌트 리뷰 반영
    
    * fix: Button 컴포넌트 리뷰 반영(2)
    
    ---------
    
    Co-authored-by: tklin <xorb811@gamil.com>

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import HomePage from "./pages/Home.page";
 import TestPage from "./pages/Test.page";
+import TestPageTK from "./pages/TestTK.page";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<TestPage />} />
+        <Route path="/tk" element={<TestPageTK />} />
         <Route path="/home" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
