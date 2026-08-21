b99b321471d5057958193ea5ee07bf12d03f945c
siwon <ksw3721@gmail.com>
2026-08-21 16:10:02 +0900

    DiscriptionCard 구현

--- src/pages/Test.page.tsx
+++ src/pages/Test.page.tsx
@@ -9,7 +9,8 @@
 import { useState } from "react";
 import TextArea from "../components/ui/TextArea";
 import CircleButton from "../components/ui/AddButton";
-import RectangleButton from "../components/ui/Button";
+import DiscriptionCard from "../components/ui/DiscriptionCard";
+import CalculatorIcon from "../assets/svg/calculator.svg?react";
 
 function TestPage() {
   const [text, setText] = useState("");
@@ -21,16 +22,19 @@ function TestPage() {
 
   return (
     <>
-      <CircleButton />
-      <div className="flex justify-center items-center h-screen bg-[lightgrey]">
-        {/* <CircleButton /> */}
+      <div className="flex flex-col gap-[10px] p-[24px] justify-center items-center h-screen bg-[lightgrey]">
+        <DiscriptionCard
+          icon={<CalculatorIcon />}
+          title="간편한 영수증 정산"
+          description="카메라로 찍거나 이미지를 올려 복잡한 영수증 내역을 즉시 분할할 수 있습니다."
+        />
         <TextArea
           name="test"
           value={text}
           onChange={(e) => setText(e.target.value)}
           placeholder="테스트 입력창"
         />
-        {/* <div className="font-normal font-[13px] border-danger border-1">test</div> */}
+        <CircleButton />
       </div>
     </>
   );
