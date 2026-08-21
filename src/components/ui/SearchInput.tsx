76a6e121007fbd233d20bbb090a3c3805d8ed4c7
madein <xorb811@gmail.com>
2026-08-21 14:17:41 +0900

    SearchInput 컴포넌트 구현 (#8)
    * feat:SearchInput 컴포넌트 구현
    
    * fix: SearchInput 컴포넌트 수정 리뷰반영
    
    * fix: 컴포넌트 name prop 추가
    
    * fix : name , value 필수 prop 변경
    
    ---------
    
    Co-authored-by: tklin <xorb811@gamil.com>

---
+++ src/components/ui/SearchInput.tsx
@@ -0,0 +1,42 @@
+import SearchIcon from "../../assets/svg/search.svg?react";
+
+type SearchInputProps = {
+  name: string;
+  value: string;
+  placeholder?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onSearch?: () => void;
+};
+
+export default function SearchInput({
+  name,
+  value,
+  placeholder,
+  onChange,
+  onSearch,
+}: SearchInputProps) {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSearch?.();
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex items-center gap-2.5 w-full h-[44px] px-4 bg-surface-sub border border-border-default rounded-[8px] focus-within:border-text-muted"
+    >
+      <button type="submit" className="cursor-pointer">
+        <SearchIcon />
+      </button>
+
+      <input
+        type="text"
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className="flex-1 bg-transparent text-body-plain text-text-primary placeholder:text-text-dim outline-none"
+      />
+    </form>
+  );
+}
