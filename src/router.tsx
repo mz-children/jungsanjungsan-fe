63fddbe7a8f604256f146cae137ee04f4b68f291
dudmstk1278 <dudmstk1278@gmail.com>
2026-09-05 09:31:38 +0900

    ReceiptItem 컴포넌트 구현 (#16)
    * ReceiptItem 파일 생성
    
    * ReceiptItem 컴포넌트 구현
    
    * ReceiptItem 확인용 TestES 페이지 및 라우트 추가
    
    * ReceiptItem 컴포넌트 기능 보완 및 목업 데이터 추가
    
    * ReceiptItem 빈 값 예외 처리 및 불필요 코드 정리

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import HomePage from "./pages/Home.page";
 import TestPage from "./pages/Test.page";
+// import TestPageTK from "./pages/TestTK.page";
+import TestES from "./pages/TestES.page";
 import TestSWPage from "./pages/TestSW.page";
 import TestPageTK from "./pages/TestTK.page";
 export default function Router() {
@@ -11,6 +13,7 @@ export default function Router() {
         <Route path="/sw" element={<TestSWPage />} />
         <Route path="/tk" element={<TestPageTK />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/es" element={<TestES />} />
       </Routes>
     </BrowserRouter>
   );
